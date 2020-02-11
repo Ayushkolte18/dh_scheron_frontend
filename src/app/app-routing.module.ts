ccd80301785de144913d0aa4f82afc484abb8c84
myusername <ayush05kolte@gmail.com>
2020-02-11 13:22:40 +0530

    updates to deploy to heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { IndustriesComponent } from './industries/industries.component';
+import { NetworkComponent } from './network/network.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'industries', component: IndustriesComponent},
+  {path: 'network', component: NetworkComponent},
+  {path: 'career', component: CareerComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { IndustriesComponent } from './industries/industries.component';
+import { NetworkComponent } from './network/network.component';
+import { ProductComponent } from './product/product.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IndustriesComponent,
+    NetworkComponent,
+    ProductComponent,
+    CareerComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
